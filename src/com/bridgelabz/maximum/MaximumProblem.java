d88647fc8b6e8fa4061d183ba5d4710b4b59d134
777shetty <shetty.chetu7@gmail.com>
2021-09-20 13:58:36 +0530

    REFACTORED:One generic method to find all three methods

--- src/com/bridgelabz/maximum/MaximumProblem.java
+++ src/com/bridgelabz/maximum/MaximumProblem.java
@@ -5,54 +5,38 @@ public class MaximumProblem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Maximum Problem Program!");
 
-		int integerNumber1 = 1, integerNumber2 = 2, integerNumber3 = 3;		
-		System.out.println("Maximum of " + integerNumber1 + ", " + integerNumber2 + ", " + integerNumber3 + " is " + findMaximum(integerNumber1, integerNumber2, integerNumber3));
-
+		int integerNumber1 = 1, integerNumber2 = 2, integerNumber3 = 3;
+		System.out.println("Maximum of " + integerNumber1 + ", " + integerNumber2 + ", " + integerNumber3 + " is "
+				+ findMaximum(integerNumber1, integerNumber2, integerNumber3));
+		
 		float floatNumber1 = 9.0f, floatNumber2 = 52.0f, floatNumber3 = 3.0f;
-		System.out.println("Maximum of " + floatNumber1 + ", " + floatNumber2 + ", " + floatNumber3 + " is " + findMaximum(floatNumber1, floatNumber2, floatNumber3));
+		System.out.println("Maximum of " + floatNumber1 + ", " + floatNumber2 + ", " + floatNumber3 + " is "
+				+ findMaximum(floatNumber1, floatNumber2, floatNumber3));
+
+		String string1 = "hello", string2 = "world", string3 = "computer";
+		System.out.println("Maximum of " + string1 + ", " + string2 + ", " + string3 + " is "
+				+ findMaximum(string1, string2, string3));
 
 	}
+	
 
-	public static int findMaximum(Integer Number1, Integer Number2, Integer Number3) {
-		if (Number1.compareTo(Number2) > 0) {
-			if (Number1.compareTo(Number3) > 0) {
-				return Number1;
+	public static <E extends Comparable<E>> E findMaximum(E firstInput, E secondInput, E thirdInput) {
+		if (firstInput.compareTo(secondInput) > 0) {
+			if (firstInput.compareTo(thirdInput) > 0) {
+				return firstInput;
 
 			} else {
-				return Number3;
+				return thirdInput;
 			}
 
 		} else {
-			if (Number2.compareTo(Number3) > 0) {
-				return Number2;
+			if (secondInput.compareTo(thirdInput) > 0) {
+				return secondInput;
 
 			} else {
-				return Number3;
-
+				return thirdInput;
 			}
-
 		}
-
 	}
-	public static float findMaximum(Float Number1, Float Number2, Float Number3) {
-		if (Number1.compareTo(Number2) > 0) {
-			if (Number1.compareTo(Number3) > 0) {
-				return Number1;
 
-			} else {
-				return Number3;
-			}
-
-		} else {
-			if (Number2.compareTo(Number3) > 0) {
-				return Number2;
-
-			} else {
-				return Number3;
-
-			}
-
-		}
-
-	}
 }
